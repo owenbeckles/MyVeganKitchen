f0f7b3352aad6ad9e57f5618d1bae8716707994b
becklesoweno@gmail.com <becklesoweno@gmail.com>
2021-06-30 16:46:56 -0400

    Schema Change

--- app/models/user.py
+++ app/models/user.py
@@ -7,6 +7,8 @@ class User(db.Model, UserMixin):
 
   id = db.Column(db.Integer, primary_key = True)
   username = db.Column(db.String(40), nullable = False, unique = True)
+  firstname = db.Column(db.String(50), nullable = False)
+  lastname = db.Column(db.String(50), nullable = False)
   email = db.Column(db.String(255), nullable = False, unique = True)
   hashed_password = db.Column(db.String(255), nullable = False)
 
