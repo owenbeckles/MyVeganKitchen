1e41671653e77de8811963912761574d77f5e4bc
becklesoweno@gmail.com <becklesoweno@gmail.com>
2021-06-30 20:06:38 -0400

    Fixing Database

--- app/models/my_meal.py
+++ app/models/my_meal.py
@@ -5,11 +5,9 @@ class My_Meal(db.Model):
     __tablename__ = 'my_meals'
 
     id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(50), nullable = False, unique = True)
-    title = db.Column(db.String(50), nullable = False, unique = True)
-    price = db.Column(db.Float(5,2), nullable = False, unique = True)
-    overview = db.Column(db.Text, nullable = False, unique = True)
-    description = db.Column(db.String(100), nullable = False, unique = True)
+    title = db.Column(db.String(50), unique = True)
+    price = db.Column(db.Float(5,2), unique = True)
+    description = db.Column(db.String(100), unique = True)
     userId = db.Column(db.Integer, db.ForeignKey("users.id"))
     mealPlanId = db.Column(db.Integer, db.ForeignKey("mealplans.id"))
 
