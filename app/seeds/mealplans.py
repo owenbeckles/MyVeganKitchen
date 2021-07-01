1479f55acd9c6069465f5c496d268cd149400149
becklesoweno@gmail.com <becklesoweno@gmail.com>
2021-07-01 18:32:18 -0400

    Finishing Seed Data for Smaller Tables

--- app/seeds/mealplans.py
+++ app/seeds/mealplans.py
@@ -5,5 +5,9 @@ def seed_mealplans():
     title='7 Day Meal Plan',
     price=4.99,
     overview='This meal plan is my go-to recommendation to new vegans, people wanting to eat more plant based or vegans who want to cut out processed foods and learn to eat more whole foods! This plan includes breakfast, lunch, dinner, and 2 snacks each day! The first 25 people who purchase this meal plan get it for free!',
-    description=''
+    description='A meal plan curated towards new vegans.'
     )
+
+    db.session.add(plan1)
+
+    db.session.commit()
