7325d90d4a2b9d49a73c2ba433531ea88d8d5245
becklesoweno@gmail.com <becklesoweno@gmail.com>
2021-07-01 18:11:08 -0400

    Blog Description Added

--- app/seeds/mealplans.py
+++ app/seeds/mealplans.py
@@ -1,3 +1,9 @@
 from app.models import db, MealPlan
 
 def seed_mealplans():
+    plan1 = MealPlan(name='7daymealplan',
+    title='7 Day Meal Plan',
+    price=4.99,
+    overview='This meal plan is my go-to recommendation to new vegans, people wanting to eat more plant based or vegans who want to cut out processed foods and learn to eat more whole foods! This plan includes breakfast, lunch, dinner, and 2 snacks each day! The first 25 people who purchase this meal plan get it for free!',
+    description=''
+    )
