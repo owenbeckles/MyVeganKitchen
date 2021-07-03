730eae304326829d70e205b416904a7ded8fc3bb
becklesoweno@gmail.com <becklesoweno@gmail.com>
2021-07-03 15:33:57 -0400

    Recipe POST Request

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, session
 from flask_login import login_required
-from app.models import User, Comment
+from app.models import User, Comment, Recipe
 
 user_routes = Blueprint('users', __name__)
 
@@ -19,27 +19,38 @@ def user(id):
     return user.to_dict()
 
 # My Kitchen
-# @user_routes.route('/mykitchen', methods = ['POST', 'DELETE'])
-# @login_required
+@user_routes.route('/mykitchen', methods = ['POST', 'DELETE'])
+@login_required
+def my_kitchen():
+    if request.method == 'POST':
+        data = request.json
+        recipeId = int(data['recipeId'])
+        current_user.recipes.append(Recipe.query.get(recipeId))
+        db.session.commit()
+        return {}
+    elif request.method == 'DELETE':
+        current_user.recipes.remove(Recipe.query.get(recipeId))
+        db.session.commit()
+        return {}
 
 # Comments
-# @user_routes.route('/recipes/<int:id>', methods = ['POST', 'PUT', 'DELETE'])
-# @login_required
-# def managing_comments(id):
-#     if request.method == 'POST':
-#         data = request.json
-#         newComment = Comment()
-#         newComment.userId = request.json['userId']
-#         newComment.recipeId = request.json['recipeId']
-#         newComment.comment = data
-#         db.session.add(newComment)
-#     elif request.method == 'PUT':
-#         comment = Comment.query.get(request.json['id'])
-#         comment.comment = request.json['comment']   
-#     elif request.method == 'DELETE':
-#         comment = Comment.query.get(request.json['id'])
-#         db.session.delete(comment)
-#     db.session.commit()
+@user_routes.route('/recipes/<int:id>', methods = ['POST', 'PUT', 'DELETE'])
+@login_required
+def managing_comments(id):
+    if request.method == 'POST':
+        data = request.json
+        newComment = Comment()
+        newComment.userId = request.json['userId']
+        newComment.recipeId = request.json['recipeId']
+        newComment.comment = data
+        db.session.add(newComment)
+    elif request.method == 'PUT':
+        comment = Comment.query.get(request.json['id'])
+        comment.comment = request.json['comment']   
+    elif request.method == 'DELETE':
+        comment = Comment.query.get(request.json['id'])
+        db.session.delete(comment)
+    db.session.commit()
 
 # Settings
 # @user_routes.route('/settings/<int:id>')
