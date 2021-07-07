f9dc8297784685838076be571a090658a86af63b
becklesoweno@gmail.com <becklesoweno@gmail.com>
2021-07-07 12:04:32 -0400

    Recipe Store

---
+++ react-app/src/store/recipe.js
@@ -0,0 +1,59 @@
+// constants
+const GET_RECIPE = 'recipe/GET_RECIPE';
+const ALL_RECIPES = 'recipe/ALL_RECIPES';
+
+
+// thunks
+const getRecipe = (recipe) => {
+    return {
+        type: GET_RECIPE,
+        payload: recipe,
+    }
+}
+
+const allRecipes = () => {
+    return {
+        type: ALL_RECIPES,
+        payload,
+    }
+}
+
+
+export const getIndividualRecipe = (id) => async (dispatch) => {
+    const res = await fetch(`/api/recipes/${id}`, {
+        method: 'GET'
+    });
+    const data = await res.json();
+    dispatch(getRecipe(data));
+}
+
+export const getAllRecipes = () => async (dispatch) => {
+    const res = await fetch('/api/recipes', {
+        method: 'GET',
+    });
+    const data = await res.json();
+    dispatch(allRecipes(data));
+}
+
+const initialState = { recipe: null };
+
+export default function reducer(state = initialState, action) {
+    let newState;
+    switch (action.type) {
+        case GET_RECIPE:
+            newState = Object.assign({}, state);
+            action.payload.forEach(recipe => {
+                newState[recipe.id] = recipe;
+            });
+            return newState;
+        case ALL_RECIPES:
+            newState = Object.assign({}, state);
+            action.payload.forEach(recipe => {
+                newState[recipe.id] = recipe;
+            });
+            return newState;
+        default:
+            return state;
+    }
+};
+
