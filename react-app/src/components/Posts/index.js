0a2f23ba191f4170270f485fd10f6b5f92feba7a
Owen Beckles <becklesoweno@gmail.com>
2021-08-17 16:03:09 -0400

    Deleting Posts Gif

--- react-app/src/components/Posts/index.js
+++ react-app/src/components/Posts/index.js
@@ -47,7 +47,7 @@ const Posts = () => {
     <div className='flex flex-col justify-between pt-5 pb-16 pl-16 mt-16' style={{backgroundColor: themeChoice.background, color: themeChoice.text}}>
         {Object.values(blog).map((post, i) => {
             return (
-                <a className={theme === 'light' ? 'hover:text-peach pb-16' : 'hover:text-avocado pb-16'} key={i} onClick={(e) => {
+                <a className={theme === 'light' ? 'hover:text-peach pb-16 cursor-pointer' : 'hover:text-avocado pb-16 cursor-pointer'} key={i} onClick={(e) => {
                     e.preventDefault();
                     window.scrollTo(0,0)
                     setactivePost(post)
